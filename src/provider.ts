af875cb2f99a20ba768c8898e0ce00aa185124fd
Casey Faber <kc@caseyfaber.com>
2021-05-03 13:37:48 -0700

    Initial commit

---
+++ src/provider.ts
@@ -0,0 +1,31 @@
+import {
+  createColorPalette,
+  FASTButton,
+  FASTDesignSystemProvider,
+  parseColorString
+} from "@microsoft/fast-components";
+import { css } from "@microsoft/fast-element";
+import {
+  defineDesignSystemProvider,
+  designSystemProperty,
+  DesignSystemProviderTemplate as template,
+  display
+} from "@microsoft/fast-foundation";
+
+@defineDesignSystemProvider({
+  name: "my-design-system-provider",
+  template,
+  styles: css`
+    ${display("block")} {
+      --body-font: "Segoe UI";
+    }
+  `
+})
+export class MyDesignSystemProvider extends FASTDesignSystemProvider {
+  @designSystemProperty({
+    default: createColorPalette(parseColorString("#FF0000"))
+  })
+  neutralPalette: any;
+}
+
+FASTButton;
